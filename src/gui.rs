facfea3a08d4e9c933cf483ae1f70e5ed83244f7
Michal Orman <michal.orman@gmail.com>
2022-03-18 23:18:50 +0100

    Added basic subcommands

---
+++ src/gui.rs
@@ -0,0 +1,48 @@
+use tuikit::{app::{App, Action}, error::Result, widget::{header::Header, footer::Footer, tabs::{Tabs, Tab}}, scene::Scene};
+
+pub fn run() -> Result<()> {
+    let mut app = App::default();
+
+    let mut header = Header::new("sttm");
+
+    let mut footer = Footer::new();
+    footer
+        .add_hotkey("tab", "switch")
+        .add_hotkey("j", "down")
+        .add_hotkey("k", "up");
+
+    let mut tabs = Tabs::new();
+    tabs
+        .add_tab(Tab::new(" Inbox"))
+        .add_tab(Tab::new(" Today"))
+        .add_tab(Tab::new(" Later"))
+        .add_tab(Tab::new(" Anytime"));
+
+    // build scene
+    let mut scene = Scene::new();
+
+    scene.add_widget(&mut footer);
+    scene.add_widget(&mut header);
+    scene.add_widget(&mut tabs);
+
+    // init the app - enter alt screen, hide cursor
+    app.init()?;
+    app.set_scene(&mut scene);
+
+    loop {
+        app.clear_screen()?;
+        app.draw()?;
+        app.flush()?;
+
+        match app.next_action()? {
+            Action::Quit => break,
+
+            _ => ()
+        }
+    }
+
+    // cleanup app - leave alt screen, show cursor
+    app.cleanup()?;
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,36 @@
-use tuikit::{app::{App, Action}, error::Result, widget::{header::Header, footer::Footer, tabs::{Tabs, Tab}}, scene::Scene};
+use clap::{Parser, Subcommand};
+use sttm::gui::run;
+use tuikit::error::Result;
+
+#[derive(Parser)]
+#[clap(version, about)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Option<Command>
+}
+
+#[derive(Subcommand)]
+enum Command {
+    #[clap(arg_required_else_help = true)]
+    Add {
+        description: Vec<String>
+    },
+
+    List,
+}
 
 fn main() -> Result<()> {
-    let mut app = App::default();
+    let cli = Cli::parse();
 
-    let mut header = Header::new("sttm");
+    if let Some(ref command) = &cli.command {
+        match command {
+            Command::Add { description } => println!("adding task: '{}'", description.join(" ")),
 
-    let mut footer = Footer::new();
-    footer
-        .add_hotkey("tab", "switch")
-        .add_hotkey("j", "down")
-        .add_hotkey("k", "up");
-
-    let mut tabs = Tabs::new();
-    tabs
-        .add_tab(Tab::new(" Inbox"))
-        .add_tab(Tab::new(" Today"))
-        .add_tab(Tab::new(" Later"))
-        .add_tab(Tab::new(" Anytime"));
-
-    // build scene
-    let mut scene = Scene::new();
-
-    scene.add_widget(&mut footer);
-    scene.add_widget(&mut header);
-    scene.add_widget(&mut tabs);
-
-    // init the app - enter alt screen, hide cursor
-    app.init()?;
-    app.set_scene(&mut scene);
-
-    loop {
-        app.clear_screen()?;
-        app.draw()?;
-        app.flush()?;
-
-        match app.next_action()? {
-            Action::Quit => break,
-
-            _ => ()
+            Command::List => println!("list tasks"),
         }
+
+        Ok(())
+    } else {
+        run()
     }
-
-    // cleanup app - leave alt screen, show cursor
-    app.cleanup()?;
-
-    Ok(())
 }
